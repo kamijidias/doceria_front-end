2288b7f98f384fd35987acd1dc87ffc917715618
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-20 19:33:35 -0300

    Criando o Document do NextJS

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,9 +1,12 @@
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div >
-      <h1>Doces :/ </h1>
-    </div>
+    <>
+    <Head>
+      <title>Doces com Amor - faça seu login</title>
+    </Head>
+    <h1>Doces com amor</h1>
+    </>
   )
 }
