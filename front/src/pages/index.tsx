a306b8184008c306f7a8d36044c454786379778d
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-24 15:31:12 -0300

    criado o contexto isAuthenticated

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,15 +1,31 @@
+import { useContext, FormEvent } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
 
 //import logoImg from '../../public/logo.svg';
 
-import {Input} from '../components/ui/Input'
+import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
+
+import { AuthContext } from '../contexts/AuthContext'
 
 import Link from 'next/link'
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: "teste@mail.com",
+      password: "123123"
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -18,24 +34,24 @@ export default function Home() {
       <div className={styles.containerCenter}>
 
         <div className={styles.login}>
-          <form>
-            <Input 
+          <form onSubmit={handleLogin}>
+            <Input
               placeholder="Digite seu email"
-              type="text"  
+              type="text"
             />
 
-            <Input 
-              placeholder="Digite sua senha"  
+            <Input
+              placeholder="Digite sua senha"
               type="password"
             />
 
             <Button
-            type="submit"
-            loading={false}
+              type="submit"
+              loading={false}
             >
               Acessar
             </Button>
-    
+
           </form>
 
           <Link href="/signup">
