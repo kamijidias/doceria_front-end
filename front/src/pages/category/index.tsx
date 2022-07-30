2846afcf489608a425632a4ee97a7f18933a4695
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-30 12:50:17 -0300

    criando a pagina de categoria

--- front/src/pages/category/index.tsx
+++ front/src/pages/category/index.tsx
@@ -6,6 +6,7 @@ import styles from './styles.module.scss'
 import { setupAPIClient } from '../../services/api'
 import { toast } from 'react-toastify'
 
+import { canSSRAuth } from '../../utils/canSSRAuth'
 
 export default function Category() {
     const [name, setName] = useState('')
@@ -58,3 +59,12 @@ export default function Category() {
         </>
     )
 }
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    return{
+        props: {}
+    }
+
+})
---
+++ front/src/pages/product/index.tsx
@@ -0,0 +1,67 @@
+
+import Head from "next/head";
+import styles from './styles.module.scss';
+import { Header } from "../../components/Header";
+
+import { canSSRAuth } from "../../utils/canSSRAuth";
+
+export default function Product() {
+    return (
+        <>
+            <Head>
+                <title>Novo produto - Doces</title>
+            </Head>
+            <div>
+                <Header />
+
+                <main className={styles.container}>
+                    <h1>Novo produto</h1>
+
+                    <form className={styles.form}>
+
+                        <select>
+                            <option>
+                                Bebida
+                            </option>
+                            <option>
+                                Doces
+                            </option>
+                        </select>
+
+                        <input
+                            type="text"
+                            placeholder="Digite o nome do produto"
+                            className={styles.input}
+                        />
+
+                        <input
+                            type="text"
+                            placeholder="Preço do produto"
+                            className={styles.input}
+                        />
+
+                        <textarea
+                            placeholder="Descreva seu produto.."
+                            className={styles.input}
+                        />
+
+                        <button className={styles.buttonAdd} type="submit">
+                            Cadastrar
+                        </button>
+
+
+                    </form>
+
+                </main>
+
+            </div>
+        </>
+    )
+}
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    return {
+        props: {}
+    }
+})
