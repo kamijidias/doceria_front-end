097ec57f70bac62618e84242f58a0d9a68465f67
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-29 22:25:34 -0300

    criando a pagina Categoria

---
+++ front/src/pages/category/index.tsx
@@ -0,0 +1,46 @@
+import { useState, FormEvent } from 'react'
+import Head from 'next/head'
+import { Header } from '../../components/Header'
+import styles from './styles.module.scss'
+import { EventEmitter } from 'stream'
+
+export default function Category() {
+    const [name, setName] = useState('')
+
+    async function handleRegister(event: FormEvent) {
+        event.preventDefault();
+
+        alert("Categoria " + name)
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Nova categoria - Doces</title>
+            </Head>
+            <div>
+                <Header />
+
+                <main className={styles.container}>
+                    <h1> Cadastrar categorias </h1>
+
+                    <form className={styles.form} onSubmit={handleRegister}>
+                        <input
+                            type="text"
+                            placeholder='Digite o nome da categoria'
+                            className={styles.input}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+
+
+                        <button className={styles.buttonAdd} type="submit">
+                            Cadastrar
+                        </button>
+
+                    </form>
+                </main>
+            </div>
+        </>
+    )
+}
