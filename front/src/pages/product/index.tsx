15664b877deb19a2a514067233e739f148e0a25f
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-30 21:27:25 -0300

    criado o elemento preview para enviar imagens

--- front/src/pages/product/index.tsx
+++ front/src/pages/product/index.tsx
@@ -1,11 +1,38 @@
-
+import { useState, ChangeEvent } from 'react'
 import Head from "next/head";
 import styles from './styles.module.scss';
 import { Header } from "../../components/Header";
 
 import { canSSRAuth } from "../../utils/canSSRAuth";
 
+import { FiUpload } from 'react-icons/fi'
+
 export default function Product() {
+
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>) {
+
+        if (!e.target.files) {
+            return;
+        }
+
+        const image = e.target.files[0];
+
+        if (!image) {
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+
+        }
+
+    }
+
     return (
         <>
             <Head>
@@ -18,6 +45,26 @@ export default function Product() {
                     <h1>Novo produto</h1>
 
                     <form className={styles.form}>
+
+                        <label className={styles.labelAvatar}>
+                            <span>
+                                <FiUpload size={30} color="#fff" />
+                            </span>
+
+                            <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+
+                            {avatarUrl && (
+                                <img
+                                    className={styles.preview}
+                                    src={avatarUrl}
+                                    alt="Foto do produto"
+                                    width={250}
+                                    height={250}
+                                />
+
+                            )}
+
+                        </label>
 
                         <select>
                             <option>
