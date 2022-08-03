33d0d276aee9fb86baf1fc1af6b3634500fdd3b4
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-08-03 17:15:34 -0300

    criado o componente Modal

--- front/src/components/ModalOrder/index.tsx
+++ front/src/components/ModalOrder/index.tsx
@@ -1,8 +1,62 @@
+import Modal from 'react-modal'
+import styles from './style.module.scss'
 
-export function ModalOrder(){
-    return(
-        <div>
-            <h1>Detalhes do pedido</h1>
-        </div>
+import { FiX } from 'react-icons/fi'
+
+import { OrderItemProps } from '../../pages/dashboard'
+
+interface ModalOrderProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+    order: OrderItemProps[];
+}
+
+export function ModalOrder({ isOpen, onRequestClose, order }: ModalOrderProps) {
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            bottom: 'auto',
+            left: '50%',
+            right: 'auto',
+            padding: '30px',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#45919b'
+        }
+    };
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            style={customStyles}
+        >
+
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+                style={{ background: 'transparent', border: 0 }}
+            >
+                <FiX size={45} color="#f34748" />
+            </button>
+
+            <div className={styles.container}>
+
+                <h2>Detalhes do pedido</h2>
+                <span className={styles.table}>
+                    Mesa: <strong>{order[0].order.table}</strong>
+                </span>
+
+                {order.map( item => (
+                    <section key={item.id} className={styles.containerItem}>
+                        <span>{item.amount} - <strong>{item.product.name}</strong></span>
+                        <span className={styles.description}>{item.product.description}</span>
+                    </section>
+                ))}
+
+            </div>
+
+        </Modal>
     )
 }
--- front/src/pages/dashboard/index.tsx
+++ front/src/pages/dashboard/index.tsx
@@ -108,7 +108,9 @@ export default function Dashboard({ orders }: HomeProps) {
 
         {modalVisible && (
           <ModalOrder
-
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            order={modalItem}
           />
         )}
 
